fa02a2537f829c5c556990c47d1b1ee6405d10e1
WarpedMind <tomgrow@gmail.com>
2017-12-22 14:47:22 -0500

    profile view and edit

--- buyusaapp/views.py
+++ buyusaapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-from .models import Gig
+from .models import Gig, Profile
 from .forms import GigForm
 
 # Create your views here.
@@ -57,4 +57,16 @@ def my_gigs(request):
 
 @login_required(login_url="/")
 def profile(request, username):
-    return render(request, 'profile.html', {})
+    if request.method == 'POST' :
+        profile = Profile.objects.get(user=request.user)
+        profile.about = request.POST['about']
+        profile.slogan = request.POST['slogan']
+        profile.save()
+    else:
+        try:
+            profile = Profile.objects.get(user__username=username)
+        except Profile.DoesNotExist:
+            return redirect('/')
+
+    gigs = Gig.objects.filter(user=profile.user, status=True)
+    return render(request, 'profile.html', {"profile":profile, "gigs": gigs})
