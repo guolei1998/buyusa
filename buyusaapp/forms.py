b660bc303ad78b90343c1e9a932887e27e2b9e4e
WarpedMind <tomgrow@gmail.com>
2018-01-28 11:49:14 -0500

    updated create_gig

--- buyusaapp/forms.py
+++ buyusaapp/forms.py
@@ -4,4 +4,6 @@ from .models import Gig
 class GigForm(ModelForm):
     class Meta:
         model = Gig
-        fields = ['title', 'category', 'description', 'price', 'photo', 'status']
+        # *** BEGIN - Update fields - TCG - 1/28/18 ***
+        # fields = ['title', 'category', 'description', 'price', 'photo', 'status']
+        fields = ['title', 'category', 'description', 'BrandLogo', 'BrandLink', 'BrandCustomerServicePhone', 'BrandSearch', 'BrandWhereToBuy', 'price', 'photo', 'BrandPicture2', 'BrandPicture3', 'BrandPicture4', 'BrandPicture5', 'status']
